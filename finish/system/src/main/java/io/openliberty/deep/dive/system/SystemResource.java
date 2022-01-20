7110bbba02e6b5ca6c2888f62b1e9c9d185ba258
Gilbert Kwan <gkwan@ca.ibm.com>
2022-01-20 18:27:28 -0500

    module-6 and system

---
+++ finish/system/src/main/java/io/openliberty/deep/dive/system/SystemResource.java
@@ -0,0 +1,54 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.deep.dive.system;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.util.Properties;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@ApplicationScoped
+@Path("/")
+public class SystemResource {
+
+	private static final MemoryMXBean MEM_BEAN = ManagementFactory.getMemoryMXBean();
+
+    @GET
+    @Path("/properties")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Properties getProperties() {
+	    return System.getProperties();
+	}
+    
+    @GET
+    @Path("/property/{property}")
+    @RolesAllowed({ "admin", "user" })
+    public String getProperty(@PathParam("property") String property) {
+	    return System.getProperty(property);
+	}
+
+    @GET
+    @Path("/heapsize")
+    @RolesAllowed({ "admin" })
+    public Long getHeapSize() {
+    	return MEM_BEAN.getHeapMemoryUsage().getMax();
+    }
+
+}
