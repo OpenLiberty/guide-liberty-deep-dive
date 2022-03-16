6e1d70370d24e5fd0ab3258340e3057ec7a96e10
Gilbert Kwan <gkwan@ca.ibm.com>
2022-03-16 17:20:44 -0400

    Dev content review

--- finish/module-jwt/src/main/java/io/openliberty/deepdive/rest/Inventory.java
+++ finish/module-jwt/src/main/java/io/openliberty/deepdive/rest/Inventory.java
@@ -12,51 +12,64 @@
 // end::copyright[]
 package io.openliberty.deepdive.rest;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import io.openliberty.deepdive.rest.model.SystemData;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @ApplicationScoped
+// tag::InventoryDao[]
 public class Inventory {
 
-    private List<SystemData> systems = Collections.synchronizedList(new ArrayList<>());
+    // tag::PersistenceContext[]
+    @PersistenceContext(name = "jpa-unit")
+    // end::PersistenceContext[]
+    private EntityManager em;
 
+    // tag::readAllFromInventory[]
     public List<SystemData> getSystems() {
-        return systems;
+        return em.createNamedQuery("SystemData.findAll", SystemData.class)
+        		 .getResultList();
     }
+    // end::readAllFromInventory[]
 
+    // tag::readInventory[]
     public SystemData getSystem(String hostname) {
-        for (SystemData s : systems) {
-            if (s.getHostname().equalsIgnoreCase(hostname)) {
-                return s;
-            }
-        }
-        return null;
+        // tag::find[]
+        List<SystemData> systems = 
+            em.createNamedQuery("SystemData.findSystem", SystemData.class)
+              .setParameter("hostname", hostname)
+              .getResultList();
+        return systems == null || systems.isEmpty() ? null : systems.get(0);
+        // end::find[]  	
     }
+    // end::readInventory[]
 
-    public boolean contains(String hostname) {
-        return getSystem(hostname) != null;
-    }
-
+    // tag::addToInventory[]
     public void add(String hostname, String osName, String javaVersion, Long heapSize) {
-        systems.add(new SystemData(hostname, osName, javaVersion, heapSize));
+        // tag::Persist[]
+        em.persist(new SystemData(hostname, osName, javaVersion, heapSize));
+        // end::Persist[]
     }
+    // end::addToInventory[]
 
-    public void update(String hostname, String osName, String javaVersion, Long heapSize) {
-        for (SystemData s : systems) {
-            if (s.getHostname().equalsIgnoreCase(hostname)) {
-                s.setOsName(osName);
-                s.setJavaVersion(javaVersion);
-                s.setHeapSize(heapSize);
-            }
-        }
+    // tag::update[]
+    public void update(SystemData s) {
+    	// tag::Merge[]
+    	em.merge(s);
+    	// end::Merge[]
     }
+    // end::update[]
 
-    public boolean removeSystem(String hostname) {
-        return systems.remove(getSystem(hostname));
+    // tag::removeSystem[]
+    public void removeSystem(SystemData s) {
+        // tag::Remove[]
+    	em.remove(s);
+    	// end::Remove[]
     }
+    // end::removeSystem[]
 
 }
+// end::InventoryDao[]
--- finish/module-jwt/src/main/java/io/openliberty/deepdive/rest/model/SystemData.java
+++ finish/module-jwt/src/main/java/io/openliberty/deepdive/rest/model/SystemData.java
@@ -12,57 +12,122 @@
 // end::copyright[]
 package io.openliberty.deepdive.rest.model;
 
-public class SystemData {
+import java.io.Serializable;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Schema(name="SystemData", description="POJO that represents a single inventory entry.")
+// tag::Entity[]
+@Entity
+// end::Entity[]
+// tag::Table[]
+@Table(name = "SystemData")
+// end::Table[]
+// tag::NamedQuery[]
+@NamedQuery(name = "SystemData.findAll", query = "SELECT e FROM SystemData e")
+@NamedQuery(name = "SystemData.findSystem", 
+    query = "SELECT e FROM SystemData e WHERE e.hostname = :hostname")
+// end::NamedQuery[]
+// tag::SystemData[]
+public class SystemData implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // tag::GeneratedValue[]
+    @SequenceGenerator(name="SEQ",sequenceName = "systemData_id_seq",allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.IDENTITY, generator="SEQ")
+    // end::GeneratedValue[]
+    // tag::Id[]
+    @Id
+    // end::Id[]
+    // tag::Column[]
+    @Column(name = "id")
+    // end::Column[]
+    private int id;
+    
+    @Schema(required = true)
+    // tag::Column[]
+    @Column(name = "hostname")
+    // end::Column[]
     private String hostname;
+
+    @Column(name = "osName")
     private String osName;
+    @Column(name = "javaVersion")
     private String javaVersion;
-    private Long   heapSize;
+    @Column(name = "heapSize")
+    private Long heapSize;
+
+    public SystemData() {
+    }
 
     public SystemData(String hostname, String osName, String javaVersion, Long heapSize) {
-        this.hostname = hostname;
-        this.osName = osName;
-        this.javaVersion = javaVersion;
-        this.heapSize = heapSize;
+		this.hostname = hostname;
+		this.osName = osName;
+		this.javaVersion = javaVersion;
+		this.heapSize = heapSize;
+	}
+
+    public int getId() {
+        return id;
     }
 
-    public String getHostname() {
-        return hostname;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setHostname(String hostname) {
-        this.hostname = hostname;
-    }
+	public String getHostname() {
+		return hostname;
+	}
 
-    public String getOsName() {
-        return osName;
-    }
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
 
-    public void setOsName(String osName) {
-        this.osName = osName;
-    }
+	public String getOsName() {
+		return osName;
+	}
 
-    public String getJavaVersion() {
-        return javaVersion;
-    }
+	public void setOsName(String osName) {
+		this.osName = osName;
+	}
 
-    public void setJavaVersion(String javaVersion) {
-        this.javaVersion = javaVersion;
-    }
+	public String getJavaVersion() {
+		return javaVersion;
+	}
 
-    public Long getHeapSize() {
-        return heapSize;
-    }
+	public void setJavaVersion(String javaVersion) {
+		this.javaVersion = javaVersion;
+	}
 
-    public void setHeapSize(Long heapSize) {
-        this.heapSize = heapSize;
+	public Long getHeapSize() {
+		return heapSize;
+	}
+
+	public void setHeapSize(Long heapSize) {
+		this.heapSize = heapSize;
+	}
+
+	@Override
+    public int hashCode() {
+        return hostname.hashCode();
     }
 
     @Override
     public boolean equals(Object host) {
-      if (host instanceof SystemData) {
-        return hostname.equals(((SystemData) host).getHostname());
-      }
-      return false;
+        if (host instanceof SystemData) {
+            return hostname.equals(((SystemData) host).getHostname());
+        }
+        return false;
     }
 }
+// end::SystemData[]
+
