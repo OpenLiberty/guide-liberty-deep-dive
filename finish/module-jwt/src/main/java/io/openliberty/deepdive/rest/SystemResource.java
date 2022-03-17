86a40b3e1c509ff0bd51601024447b593301fad5
Gilbert Kwan <gkwan@ca.ibm.com>
2022-03-17 10:58:16 -0400

    Dev content review

--- finish/module-jwt/src/main/java/io/openliberty/deepdive/rest/SystemResource.java
+++ finish/module-jwt/src/main/java/io/openliberty/deepdive/rest/SystemResource.java
@@ -15,9 +15,18 @@ package io.openliberty.deepdive.rest;
 import java.net.URI;
 import java.util.List;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.ParameterIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameters;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.jwt.JsonWebToken;
-import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
 import io.openliberty.deepdive.rest.client.SystemClient;
 import io.openliberty.deepdive.rest.client.UnknownUriExceptionMapper;
@@ -25,17 +34,16 @@ import io.openliberty.deepdive.rest.model.SystemData;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -49,13 +57,22 @@ public class SystemResource {
     @Inject
     @ConfigProperty(name = "client.https.port")
     String CLIENT_PORT;
-    
+
+    // tag::jwt[]
     @Inject
     JsonWebToken jwt;
-    
+    // end::jwt[]
+
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
+    @APIResponseSchema(value = SystemData.class,
+        responseDescription = "A list of system data stored within the inventory.",
+        responseCode = "200")
+    @Operation(
+        summary = "List contents.",
+        description = "Returns the currently stored system data in the inventory.",
+        operationId = "listContents")
     public List<SystemData> listContents() {
         return inventory.getSystems();
     }
@@ -63,65 +80,231 @@ public class SystemResource {
     @GET
     @Path("/{hostname}")
     @Produces(MediaType.APPLICATION_JSON)
-    public SystemData getSystem(@PathParam("hostname") String hostname) {
+    @APIResponseSchema(value = SystemData.class,
+        responseDescription = "JVM system properties of a particular host.",
+        responseCode = "200")
+    @Operation(
+        summary = "Get System",
+        description = "Retrieves and returns the JVM system properties from the system "
+        + "service running on the particular host.",
+        operationId = "getSystem"
+    )
+    public SystemData getSystem(
+        @Parameter(
+            name = "hostname", in = ParameterIn.PATH,
+            description = "The hostname of the system",
+            required = true, example = "localhost",
+            schema = @Schema(type = SchemaType.STRING)
+        )
+        @PathParam("hostname") String hostname) {
     	return inventory.getSystem(hostname);
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addSystem(@FormParam("hostname") String hostname,
-        @FormParam("osName") String osName,
-        @FormParam("javaVersion") String javaVersion,
-        @FormParam("heapSize") Long heapSize) {
+    @Transactional
+    @APIResponses(value={
+        @APIResponse(
+            responseCode = "200",
+            description = "Successfully added system to inventory"
+        ),
+        @APIResponse(
+           responseCode = "400",
+            description = "Unable to add system to inventory" 
+        )
+    })
+    @Parameters(value={
+        @Parameter(
+            name = "hostname", in = ParameterIn.QUERY,
+            description = "The hostname of the system",
+            required = true, example = "localhost",
+            schema = @Schema(type = SchemaType.STRING)
+        ),
+        @Parameter(
+            name = "osName", in = ParameterIn.QUERY,
+            description = "The operating system of the system",
+            required = true, example = "linux",
+            schema = @Schema(type = SchemaType.STRING)
+        ),
+        @Parameter(
+            name = "javaVersion", in = ParameterIn.QUERY,
+            description = "The Java version of the system",
+            required = true, example = "11",
+            schema = @Schema(type = SchemaType.STRING)
+        ),
+        @Parameter(
+            name = "heapSize", in = ParameterIn.QUERY,
+            description = "The heap size of the system",
+            required = true, example = "1048576",
+            schema = @Schema(type = SchemaType.NUMBER)
+        ),
+    })
+    @Operation(
+        summary = "Add system",
+        description = "This adds a system and its properties to the inventory list of systems.",
+        operationId = "addSystem"
+    )
+    public Response addSystem(
+        @QueryParam("hostname") String hostname,
+        @QueryParam("osName") String osName,
+        @QueryParam("javaVersion") String javaVersion,
+        @QueryParam("heapSize") Long heapSize) {
 
-        if (inventory.contains(hostname)) {
+    	SystemData s = inventory.getSystem(hostname);
+        if (s != null) {
             return fail(hostname + " already exists.");
         }
         inventory.add(hostname, osName, javaVersion, heapSize);
         return success(hostname + " was added.");
     }
 
+    // tag::put[]
     @PUT
+    // end::put[]
+    // tag::putEndpoint[]
     @Path("/{hostname}")
+    // end::putEndpoint[]
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    // tag::putRolesAllowed[]
     @RolesAllowed({ "admin", "user" })
-    public Response updateSystem(@PathParam("hostname") String hostname,
-        @FormParam("osName") String osName,
-        @FormParam("javaVersion") String javaVersion,
-        @FormParam("heapSize") Long heapSize) {
+    // end::putRolesAllowed[]
+    @APIResponses(value={
+        @APIResponse(
+            responseCode = "200",
+            description = "Successfully updated system"
+        ),
+        @APIResponse(
+           responseCode = "400",
+           description = "Unable to update system, as this system does not exist in the inventory list" 
+        )
+    })
+    @Parameters(value={
+        @Parameter(
+            name = "hostname", in = ParameterIn.PATH,
+            description = "The hostname of the system",
+            required = true, example = "localhost",
+            schema = @Schema(type = SchemaType.STRING)
+        ),
+        @Parameter(
+            name = "osName", in = ParameterIn.QUERY,
+            description = "The operating system of the system",
+            required = true, example = "linux",
+            schema = @Schema(type = SchemaType.STRING)
+        ),
+        @Parameter(
+            name = "javaVersion", in = ParameterIn.QUERY,
+            description = "The Java version of the system",
+            required = true, example = "11",
+            schema = @Schema(type = SchemaType.STRING)
+        ),
+        @Parameter(
+            name = "heapSize", in = ParameterIn.QUERY,
+            description = "The heap size of the system",
+            required = true, example = "1048576",
+            schema = @Schema(type = SchemaType.NUMBER)
+        ),
+    })
+    @Operation(
+        summary = "Update system",
+        description = "This updates a system and its properties on the inventory list of systems.",
+        operationId = "updateSystem"
+    )
+    public Response updateSystem(
+        @PathParam("hostname") String hostname,
+        @QueryParam("osName") String osName,
+        @QueryParam("javaVersion") String javaVersion,
+        @QueryParam("heapSize") Long heapSize) {
 
-        if (!inventory.contains(hostname)) {
+    	SystemData s = inventory.getSystem(hostname);
+        if (s == null) {
             return fail(hostname + " does not exists.");
         }
-        inventory.update(hostname, osName, javaVersion, heapSize);
+        s.setOsName(osName);
+        s.setJavaVersion(javaVersion);
+        s.setHeapSize(heapSize);
+        inventory.update(s);
         return success(hostname + " was updated.");
     }
 
+    // tag::delete[]
     @DELETE
+    // end::delete[]
+    // tag::deleteEndpoint[]
     @Path("/{hostname}")
+    // end::deleteEndpoint[]
     @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    // tag::deleteRolesAllowed[]
     @RolesAllowed({ "admin" })
+    // end::deleteRolesAllowed[]
+    @APIResponses(value={
+        @APIResponse(
+            responseCode = "200",
+            description = "Successfully deleted system from inventory"
+        ),
+        @APIResponse(
+           responseCode = "400",
+            description = "Unable to delete system from inventory, as this system does not exist" 
+        )
+    })
+    @Parameter(
+        name = "hostname", in = ParameterIn.PATH,
+        description = "The hostname of the system",
+        required = true, example = "localhost",
+        schema = @Schema(type = SchemaType.STRING)
+    )
+    @Operation(
+        summary = "Remove system",
+        description = "This removes a system and its properties from the inventory list of systems.",
+        operationId = "removeSystem"
+    )
     public Response removeSystem(@PathParam("hostname") String hostname) {
-        if (inventory.removeSystem(hostname)) {
+    	SystemData s = inventory.getSystem(hostname);
+        if (s != null) {
+        	inventory.removeSystem(s);
             return success(hostname + " was removed.");
         } else {
             return fail(hostname + " does not exists.");
         }
     }
-
-    // tag::addSystemClient[]
+    
     @POST
     @Path("/client/{hostname}")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
     @RolesAllowed({ "admin" })
-    public Response addSystemClient(@PathParam("hostname") String hostname, @Context HttpServletRequest request) {
-    	
-        if (inventory.contains(hostname)) {
+    @APIResponses(value={
+        @APIResponse(
+            responseCode = "200",
+            description = "Successfully added system client"
+        ),
+        @APIResponse(
+           responseCode = "400",
+            description = "Unable to add system client" 
+        )
+    })
+    @Parameter(
+        name = "hostname", in = ParameterIn.PATH,
+        description = "The hostname of the system",
+        required = true, example = "localhost",
+        schema = @Schema(type = SchemaType.STRING)
+    )
+    @Operation(
+        summary = "Add system client",
+        description = "This adds a system client.",
+        operationId = "addSystemClient"
+    )
+    public Response addSystemClient(@PathParam("hostname") String hostname) {
+
+    	SystemData s = inventory.getSystem(hostname);
+        if (s != null) {
             return fail(hostname + " already exists.");
         }
+        
         SystemClient customRestClient = null;
         try {
             customRestClient = getSystemClient(hostname);
@@ -129,7 +312,7 @@ public class SystemResource {
             return fail("Failed to create the client " + hostname + ".");
         }
 
-        // tag::jwt[]
+        // tag::callSystem[]
         String authHeader = "Bearer " + jwt.getRawToken();
         try {
             // tag::customRestClient[]
@@ -142,9 +325,8 @@ public class SystemResource {
             return fail("Failed to reach the client " + hostname + ".");
         }
         return success(hostname + " was added.");
-        // end::jwt[]
+        // end::callSystem[]
     }
-    // end::addSystemClient[]
 
     // tag::getSystemClient[]
     private SystemClient getSystemClient(String hostname) throws Exception {
@@ -166,5 +348,4 @@ public class SystemResource {
                        .entity("{ \"error\" : \"" + message + "\" }")
                        .build();
     }
-
 }
