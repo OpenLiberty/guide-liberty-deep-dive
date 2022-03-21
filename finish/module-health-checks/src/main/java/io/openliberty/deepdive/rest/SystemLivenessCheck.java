661fbed5fbcb045f64fe70db12a2de435972ee0e
Rutav Shah <33077901+rutavshah@users.noreply.github.com>
2022-03-21 17:44:17 -0400

    Added health module with updated README

---
+++ finish/module-health-checks/src/main/java/io/openliberty/deepdive/rest/SystemLivenessCheck.java
@@ -0,0 +1,43 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018, 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+// tag::SystemLivenessCheck[]
+package io.openliberty.deepdive.rest;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+// tag::Liveness[]
+@Liveness
+// end::Liveness[]
+@ApplicationScoped
+public class SystemLivenessCheck implements HealthCheck {
+
+  @Override
+  public HealthCheckResponse call() {
+    MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
+    long memUsed = memBean.getHeapMemoryUsage().getUsed();
+    long memMax = memBean.getHeapMemoryUsage().getMax();
+
+    return HealthCheckResponse.named(
+      SystemResource.class.getSimpleName() + " Liveness Check")
+                              .withData("memory used", memUsed)
+                              .withData("memory max", memMax)
+                              .status(memUsed < memMax * 0.9).build();
+  }
+}
+// end::SystemLivenessCheck[]
---
+++ finish/module-health-checks/src/main/java/io/openliberty/deepdive/rest/SystemReadinessCheck.java
@@ -0,0 +1,54 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018, 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+// tag::SystemReadinessCheck[]
+package io.openliberty.deepdive.rest;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+
+import java.io.IOException;
+import java.net.Socket;
+
+
+// tag::Readiness[]
+@Readiness
+// end::Readiness[]
+// tag::ApplicationScoped[]
+@ApplicationScoped
+// end::ApplicationScoped[]
+public class SystemReadinessCheck implements HealthCheck {
+
+  private String host = "localhost";
+  private int port = 5432;
+
+  @Override
+  public HealthCheckResponse call() {
+    HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Readiness Health Check");
+    
+    try {
+      connectToServer(host, port);
+      responseBuilder.up();
+    } catch (Exception e) {
+      responseBuilder.down();
+    }
+    return responseBuilder.build();
+  }
+
+  private void connectToServer(String dbhost, int port) throws IOException {
+    Socket socket = new Socket(dbhost, port);
+    socket.close();
+  }
+}
