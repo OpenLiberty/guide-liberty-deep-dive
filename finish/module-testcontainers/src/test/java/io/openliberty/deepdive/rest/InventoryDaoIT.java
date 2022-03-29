4f41dd1bc497ed9a2fb00b1d69ccbc34415dbb87
Jamie Coleman3 <jlcoleman@uk.ibm.com>
2022-03-29 21:29:42 +0100

    add in licence header to test classes

--- finish/module-testcontainers/src/test/java/io/openliberty/deepdive/rest/InventoryDaoIT.java
+++ finish/module-testcontainers/src/test/java/io/openliberty/deepdive/rest/InventoryDaoIT.java
@@ -1,3 +1,15 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018, 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
 package io.openliberty.deepdive.rest;
 
 //imports for testing
@@ -125,4 +137,5 @@ public class InventoryDaoIT {
         assertNull(inv.getName());
         System.out.println("Inventory delete attempted!");
     }
+    
 }
