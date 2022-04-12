6886ada654c2ccd9ebaa7806993b2cd550e84aa4
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-12 17:35:42 -0400

    dev content review

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -13,42 +13,35 @@ mvn -Dhttp.keepAlive=false \
 
 mvn liberty:start
 
-curl -s http://localhost:9080/inventory/api/systems | grep \\[\\]
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
-curl -s http://localhost:9080/inventory/api/systems | grep \\[\\]
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 200 ] ; then exit $?; fi
 
-curl -s http://localhost:9080/inventory/api/systems | grep localhost
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 400 ] ; then exit $?; fi
 
-curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/localhost?osName=mac&javaVersion=17&heapSize=2")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 200 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/unknown?osName=mac&javaVersion=17&heapSize=2")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 400 ] ; then exit $?; fi
 
-curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists"
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists" || echo failed; exit 1
 
-curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep removed
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep removed || echo failed; exit 1
 
-curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "not implemented"
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "not implemented" || echo failed; exit 1
 
 mvn liberty:stop
 
@@ -61,42 +54,36 @@ mvn -Dhttp.keepAlive=false \
 
 mvn liberty:start
 
-curl -s http://localhost:9080/inventory/api/systems | grep \\[\\]
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
-curl -s http://localhost:9080/inventory/api/systems | grep \\[\\]
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 200 ] ; then exit $?; fi
 
-curl -s http://localhost:9080/inventory/api/systems | grep localhost
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 400 ] ; then exit $?; fi
 
 curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost
 if [ $? -gt 0 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/localhost?osName=mac&javaVersion=17&heapSize=2")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 200 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/unknown?osName=mac&javaVersion=17&heapSize=2")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 400 ] ; then exit $?; fi
 
-curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists"
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists" || echo failed; exit 1
 
-curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep removed
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep removed || echo failed; exit 1
 
-curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "not implemented"
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "not implemented" || echo failed; exit 1
 
 mvn liberty:stop
 
@@ -112,41 +99,34 @@ mvn -Dhttp.keepAlive=false \
 
 mvn liberty:start
 
-curl -s http://localhost:9080/inventory/api/systems | grep \\[\\]
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
-curl -s http://localhost:9080/inventory/api/systems | grep \\[\\]
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 200 ] ; then exit $?; fi
 
-curl -s http://localhost:9080/inventory/api/systems | grep localhost
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 400 ] ; then exit $?; fi
 
-curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/localhost?osName=mac&javaVersion=17&heapSize=2")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 200 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/unknown?osName=mac&javaVersion=17&heapSize=2")"
-echo status=$status
+echo status='$status'
 if [ $status -ne 400 ] ; then exit $?; fi
 
-curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists"
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists" || echo failed; exit 1
 
-curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep removed
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep removed || echo failed; exit 1
 
-curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "5555"
-if [ $? -gt 0 ] ; then exit $?; fi
+curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "5555" || echo failed; exit 1
 
 mvn liberty:stop
