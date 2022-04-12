5a86f2f5e287bf4cabc212859a08073a5dc695e6
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-12 17:39:55 -0400

    dev content review

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -18,24 +18,24 @@ curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo fail
 curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status='$status'
-if [ $status -ne 200 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 200 ] ; then exit $?; fi
 
 curl -s http://localhost:9080/inventory/api/systems | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status='$status'
-if [ $status -ne 400 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 400 ] ; then exit $?; fi
 
 curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/localhost?osName=mac&javaVersion=17&heapSize=2")"
-echo status='$status'
-if [ $status -ne 200 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 200 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/unknown?osName=mac&javaVersion=17&heapSize=2")"
-echo status='$status'
-if [ $status -ne 400 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 400 ] ; then exit $?; fi
 
 curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists" || echo failed; exit 1
 
@@ -59,25 +59,25 @@ curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo fail
 curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status='$status'
-if [ $status -ne 200 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 200 ] ; then exit $?; fi
 
 curl -s http://localhost:9080/inventory/api/systems | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status='$status'
-if [ $status -ne 400 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 400 ] ; then exit $?; fi
 
 curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost
 if [ $? -gt 0 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/localhost?osName=mac&javaVersion=17&heapSize=2")"
-echo status='$status'
-if [ $status -ne 200 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 200 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/unknown?osName=mac&javaVersion=17&heapSize=2")"
-echo status='$status'
-if [ $status -ne 400 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 400 ] ; then exit $?; fi
 
 curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists" || echo failed; exit 1
 
@@ -104,24 +104,24 @@ curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo fail
 curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status='$status'
-if [ $status -ne 200 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 200 ] ; then exit $?; fi
 
 curl -s http://localhost:9080/inventory/api/systems | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X POST "http://localhost:9080/inventory/api/systems?hostname=localhost&osName=mac&javaVersion=11&heapSize=1")"
-echo status='$status'
-if [ $status -ne 400 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 400 ] ; then exit $?; fi
 
 curl -s http://localhost:9080/inventory/api/systems/localhost | grep localhost || echo failed; exit 1
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/localhost?osName=mac&javaVersion=17&heapSize=2")"
-echo status='$status'
-if [ $status -ne 200 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 200 ] ; then exit $?; fi
 
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null -X PUT "http://localhost:9080/inventory/api/systems/unknown?osName=mac&javaVersion=17&heapSize=2")"
-echo status='$status'
-if [ $status -ne 400 ] ; then exit $?; fi
+echo status="$status"
+if [ "$status" -ne 400 ] ; then exit $?; fi
 
 curl -X DELETE http://localhost:9080/inventory/api/systems/unknown | grep "does not exists" || echo failed; exit 1
 
