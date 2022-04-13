373b83f615f619b5a3bbd6a8383a4e47c4856098
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-13 12:09:13 -0400

    fix finishMetrics.sh

--- scripts/finishMetrics.sh
+++ scripts/finishMetrics.sh
@@ -9,5 +9,5 @@ mkdir -p ./start/inventory/src/main/liberty/config/resources/security
 cp ./finish/system/src/main/liberty/config/resources/security/key.p12 ./start/inventory/src/main/liberty/config/resources/security/key.p12
 mkdir ./start/inventory/src/main/java/io/openliberty/deepdive/rest/health
 cp ./finish/module-health-checks/src/main/java/io/openliberty/deepdive/rest/health/*.java ./start/inventory/src/main/java/io/openliberty/deepdive/rest/health
-cp ./finish/system/src/main/liberty/config/server.xml ./start/inventory/src/main/liberty/config
+cp ./finish/module-metrics/src/main/liberty/config/server.xml ./start/inventory/src/main/liberty/config
 cp ./finish/module-metrics/src/main/java/io/openliberty/deepdive/rest/SystemResource.java ./start/inventory/src/main/java/io/openliberty/deepdive/rest
