7034b44321c29eb193451fc449cd94da5816fc59
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-19 12:39:58 -0400

    update finishContainer.sh

--- scripts/finishContainer.sh
+++ scripts/finishContainer.sh
@@ -18,6 +18,10 @@ cd ./start/inventory || exit
 mvn clean package liberty:create liberty:install-feature liberty:deploy
 docker build -t liberty-deepdive-inventory:1.0-SNAPSHOT .
 
+if [[ -e /home/project ]]; then
+    docker tag liberty-deepdive-inventory:1.0-SNAPSHOT us.icr.io/$SN_ICR_NAMESPACE/liberty-deepdive-inventory:1.0-SNAPSHOT
+fi
+
 cd ../../finish/postgres || exit
 docker build -t postgres-sample .
 
