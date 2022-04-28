de54dfa4ec9be1aff427ef2507c774a197f5aa0f
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-28 15:46:57 -0400

    Create dailyBuild.sh

---
+++ scripts/dailyBuild.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+while getopts t:d:b:u: flag; do
+    case "${flag}" in
+    t) DATE="${OPTARG}" ;;
+    d) DRIVER="${OPTARG}" ;;
+    *) echo "Invalid option" ;;
+    esac
+done
+
+echo "Testing daily build image"
+
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml
+sed -n 50,70p system/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" module-getting-started/pom.xml
+sed -n 50,70p module-getting-started/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" module-openapi/pom.xml
+sed -n 50,70p module-openapi/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" module-config/pom.xml
+sed -n 50,70p module-config/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" module-persisting-data/pom.xml
+sed -n 50,70p module-persisting-data/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" module-securing/pom.xml
+sed -n 50,70p module-securing/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" module-jwt/pom.xml
+sed -n 50,70p module-jwt/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" module-testcontainers/pom.xml
+sed -n 110,130p module-testcontainers/pom.xml
+
+sudo ../scripts/testApp.sh
