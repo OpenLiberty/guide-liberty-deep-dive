09f63dc65d443b151ab2f9a7d4d78bc4c3983539
Gilbert Kwan <gkwan@ca.ibm.com>
2022-05-13 16:55:52 -0400

    fix testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -128,9 +128,22 @@ mvn liberty:stop
 MULTILINE-COMMENT
 
 echo ===== Test module-jwt =====
+
+cd ../postgres || exit
+docker build -t postgres-sample .
+docker run --name postgres-container -p 5432:5432 -d postgres-sample
+
+cd ../system || exit
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -q clean package liberty:create liberty:install-feature liberty:deploy
+mvn liberty:start
+
 cd ../module-jwt || exit
 
-mkdir ./src/main/java/io/openliberty/deepdive/rest/health
+cp -f ../system/src/main/liberty/config/resources/security/key.p12 ./src/main/liberty/config/resources/security/key.p12
+mkdir -p ./src/main/java/io/openliberty/deepdive/rest/health
 cp ../module-health-checks/src/main/java/io/openliberty/deepdive/rest/health/StartupCheck.java ./src/main/java/io/openliberty/deepdive/rest/health
 cp ../module-health-checks/src/main/java/io/openliberty/deepdive/rest/health/LivenessCheck.java ./src/main/java/io/openliberty/deepdive/rest/health
 cp ../module-health-checks/src/main/java/io/openliberty/deepdive/rest/health/ReadinessCheck.java ./src/main/java/io/openliberty/deepdive/rest/health
@@ -144,17 +157,7 @@ mvn -Dhttp.keepAlive=false \
 
 mvn liberty:start
 
-cd ../postgres || exit
-docker build -t postgres-sample .
-docker run --name postgres-container -p 5432:5432 -d postgres-sample
-
-cd ../system || exit
-
-mvn liberty:start
-
-curl -k --user bob:bobpwd -X POST 'https://localhost:9443/inventory/api/systems/client/localhost' | grep "was added" || exit 1
-
-curl 'http://localhost:9080/inventory/api/systems' | grep "\"heapSize\":" || exit 1
+sleep 20
 
 echo ===== Test module-health-checks =====
 
@@ -164,6 +167,14 @@ curl http://localhost:9080/health/started | grep "\"status\":" || exit 1
 curl http://localhost:9080/health/live | grep "\"status\":" || exit 1
 curl http://localhost:9080/health/ready | grep "\"status\":\"UP\"" || exit 1
 
+
+echo ===== Test client REST API =====
+
+curl -k --user bob:bobpwd -X POST 'https://localhost:9443/inventory/api/systems/client/localhost' | grep "was added" || exit 1
+
+curl 'http://localhost:9080/inventory/api/systems' | grep "\"heapSize\":" || exit 1
+
+
 echo ===== Test module-metrics =====
 
 curl -k --user bob:bobpwd -X DELETE https://localhost:9443/inventory/api/systems/localhost
@@ -172,6 +183,9 @@ curl -k --user alice:alicepwd -X PUT http://localhost:9080/inventory/api/systems
 curl -s http://localhost:9080/inventory/api/systems
 
 curl -k --user bob:bobpwd https://localhost:9443/metrics/application | grep 'application_addSystemClient_total 0\|application_addSystem_total 1\|application_updateSystem_total 1\|application_removeSystem_total 1' || exit 1
+
+
+echo ===== Stop all processes
 
 mvn liberty:stop 
 
