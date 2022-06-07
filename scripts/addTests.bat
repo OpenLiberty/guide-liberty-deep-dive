2748a97b2fa5c3d2c4bb1840db625ca06f637e76
Dylan Uhryniuk <dylan.uhryniuk@ibm.com>
2022-06-07 13:12:28 -0400

    Copy tests to correct directory

--- scripts/addTests.bat
+++ scripts/addTests.bat
@@ -4,6 +4,6 @@ if exist ".\start\inventory\src\test" (
     rmdir /s /q ".\start\inventory\src\test"
 )
 
-robocopy %CD%\finish\module-testcontainers\src\test\ %CD%\start\inventory\src\ *.* /e /NFL /NDL /NJH /NJS /nc /ns /np
+robocopy %CD%\finish\module-testcontainers\src\test\ %CD%\start\inventory\src\test *.* /e /NFL /NDL /NJH /NJS /nc /ns /np
 
 copy ".\finish\module-testcontainers\pom.xml" ".\start\inventory\pom.xml" >NUL
