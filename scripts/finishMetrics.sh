5ad9d5b331894f0147496e8e29a5f8c230517f55
dylan <74523230+uhryniuk@users.noreply.github.com>
2022-06-08 06:22:38 -0700

    Windows version of scripts (#116)
    * Windows version of scripts
    
    * Correct foreslash to backslash
    
    * Ensure scripts return to root after completed.
    
    * Additional batch and bash scripts
    
    * Additions based on comments
    
    * Updating scripts
    
    * Stopping system and postgres in finishContainer.sh/bat
    
    * Copy tests to correct directory
    
    * Update finishContainer.sh
    
    * Update finishContainer.bat
    
    * Update CONTRIBUTING.md
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    Co-authored-by: Dylan Uhryniuk <dylan.uhryniuk@ibm.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/finishMetrics.sh
+++ scripts/finishMetrics.sh
@@ -3,6 +3,7 @@
 if [[ -e ./start/inventory ]]; then
     rm -fr ./start/inventory
 fi
+
 mkdir ./start/inventory
 cp -fr ./finish/module-jwt/* ./start/inventory
 mkdir -p ./start/inventory/src/main/liberty/config/resources/security
@@ -11,6 +12,10 @@ mkdir ./start/inventory/src/main/java/io/openliberty/deepdive/rest/health
 cp ./finish/module-health-checks/src/main/java/io/openliberty/deepdive/rest/health/*.java ./start/inventory/src/main/java/io/openliberty/deepdive/rest/health
 cp ./finish/module-metrics/src/main/liberty/config/server.xml ./start/inventory/src/main/liberty/config
 cp ./finish/module-metrics/src/main/java/io/openliberty/deepdive/rest/SystemResource.java ./start/inventory/src/main/java/io/openliberty/deepdive/rest
-echo Now, you may run following commands to continue the class:
+
+./scripts/startSystem.sh
+./scripts/startPostgres.sh
+
+echo Now, you may run following commands to continue the tutorial:
 echo cd start/inventory
 echo mvn liberty:dev -DserverStartTimeout=120
--- scripts/finishPersistingData.sh
+++ scripts/finishPersistingData.sh
@@ -4,7 +4,10 @@ if [[ -e ./start/inventory ]]; then
     rm -fr ./start/inventory
 fi
 mkdir ./start/inventory
-cp -fr ./finish/module-config/* ./start/inventory
+cp -fr ./finish/module-persisting-data/* ./start/inventory
+
+./scripts/startPostgres.sh
+
 echo Now, you may run following commands to continue the tutorial:
 echo cd start/inventory
 echo mvn liberty:dev -DserverStartTimeout=120
