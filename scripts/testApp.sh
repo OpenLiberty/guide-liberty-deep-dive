c7e5fca81c1d3e5a03ed4db9a3440937afaa42ef
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-15 14:35:07 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -236,7 +236,7 @@ cp ../module-kubernetes/inventory.init.yaml .
 cp ../module-kubernetes/inventory.yaml .
 
 #./../scripts/startMinikube.sh
-minikube start --memory 7168
+minikube start --memory 6144
 minikube status
 #kubectl cluster-info
 #kubectl get services --all-namespaces
