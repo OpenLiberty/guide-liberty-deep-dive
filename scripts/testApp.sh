cf2629b91272514fbaed1a3d7410438a00b8ec02
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-16 14:55:55 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -267,13 +267,17 @@ kubectl create configmap inv-app-root --from-literal contextRoot=/dev
 sleep 120
 
 kubectl get pods
+kubectl describe pods
 
-kubectl port-forward --address "$(minikube ip)" svc/inventory-deployment 9443:9443 &
+#kubectl port-forward --address "$(minikube ip)" svc/inventory-deployment 9443:9443 &
+kubectl port-forward --address "$(minikube ip)" svc/inventory-deployment 9080:8080 &
 
 sleep 120
 
-curl -k -X POST "https://$(minikube ip):9443/dev/api/systems?heapSize=1048576&hostname=localhost&javaVersion=9&osName=linux" | grep "added" || exit 1
-curl -k "https://$(minikube ip):9443/dev/api/systems" | grep "localhost" || exit 1
+#curl -k -X POST "https://$(minikube ip):9443/dev/api/systems?heapSize=1048576&hostname=localhost&javaVersion=9&osName=linux" | grep "added" || exit 1
+#curl -k "https://$(minikube ip):9443/dev/api/systems" | grep "localhost" || exit 1
+curl -k -X POST "https://localhost:9080/dev/api/systems?heapSize=1048576&hostname=localhost&javaVersion=9&osName=linux" | grep "added" || exit 1
+curl -k "https://localhost:9080/dev/api/systems" | grep "localhost" || exit 1
 
 kubectl delete -f inventory.yaml
 kubectl delete -f ../postgres/postgres.yaml
