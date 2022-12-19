e31996d3ab9bfbe7faae88a1ee3934256f301079
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-19 11:32:32 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -273,6 +273,7 @@ minikube kubectl port-forward svc/inventory-deployment 9443 &
 
 sleep 20
 
+curl -q -k "https://localhost:9443/dev/api/systems"
 curl -q -k -X POST "https://localhost:9443/dev/api/systems?heapSize=1048576&hostname=localhost&javaVersion=9&osName=linux" | grep "added" || exit 1
 curl -q -k "https://localhost:9443/dev/api/systems" | grep "localhost" || exit 1
 
