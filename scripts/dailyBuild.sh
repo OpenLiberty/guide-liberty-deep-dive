70e9512a55868b7b09ed920d302d1e79740e7fa0
Gilbert Kwan <gkwan@ca.ibm.com>
2024-01-17 11:02:15 -0500

    Update dailyBuild.sh (#212)

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-while getopts t:d:b:u: flag; do
+while getopts t:d:b:u:j: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
     b) BUILD="${OPTARG}";;
     u) DOCKER_USERNAME="${OPTARG}";;
+    j) JDK_LEVEL="${OPTARG}" ;;
     *) echo "Invalid option" ;;
     esac
 done
 
 echo "Testing daily build image"
+
+if [ "$JDK_LEVEL" == "11" ]; then
+    echo "Test skipped because the guide does not support Java 11."
+    exit 0
+fi
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml module-getting-started/pom.xml module-openapi/pom.xml module-config/pom.xml
 cat system/pom.xml
