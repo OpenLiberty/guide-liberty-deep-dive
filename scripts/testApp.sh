43a8b5abd712d91c8e3a582e658861140441d3a3
George Zhang <geetransit@gmail.com>
2025-05-20 10:37:47 -0400

    Revert mvnw for test script

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
-./mvnw -version
 
 
 echo ===== Test module-getting-started =====
+cd module-getting-started || exit
 
-./mvnw -ntp -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q -f module-getting-started/pom.xml clean package liberty:create liberty:install-feature liberty:deploy
+    -q clean package liberty:create liberty:install-feature liberty:deploy
 
-./mvnw -ntp -f module-getting-started/pom.xml liberty:start
+mvn -ntp liberty:start
 curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || exit 1
 
 curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || exit 1
@@ -41,16 +41,16 @@ curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep remo
 
 curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "not implemented" || exit 1
 
-./mvnw -ntp -f module-getting-started/pom.xml liberty:stop
+mvn -ntp liberty:stop
 
 echo ===== Test module-openapi =====
-
-./mvnw -ntp -Dhttp.keepAlive=false \
+cd ../module-openapi || exit
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q -f module-openapi/pom.xml clean package liberty:create liberty:install-feature liberty:deploy
+    -q clean package liberty:create liberty:install-feature liberty:deploy
 
-./mvnw -ntp -f module-openapi/pom.xml liberty:start
+mvn -ntp liberty:start
 
 curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || exit 1
 
@@ -82,16 +82,16 @@ curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep remo
 
 curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "not implemented" || exit 1
 
-./mvnw -ntp -f module-openapi/pom.xml liberty:stop
+mvn -ntp liberty:stop
 
 echo ===== Test module-config =====
-
-./mvnw -ntp -Dhttp.keepAlive=false \
+cd ../module-config || exit
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q -f module-config/pom.xml clean package liberty:create liberty:install-feature liberty:deploy
+    -q clean package liberty:create liberty:install-feature liberty:deploy
 
-./mvnw -ntp -f module-config/pom.xml liberty:start
+mvn -ntp liberty:start
 
 curl -s http://localhost:9080/inventory/api/systems | grep "\\[\\]" || exit 1
 
@@ -123,22 +123,22 @@ curl -X DELETE http://localhost:9080/inventory/api/systems/localhost | grep remo
 
 curl -X POST http://localhost:9080/inventory/api/systems/client/localhost | grep "5555" || exit 1
 
-./mvnw -ntp -f module-config/pom.xml liberty:stop
+mvn -ntp liberty:stop
 
 echo ===== Test module-jwt =====
 
-cd postgres || exit
+cd ../postgres || exit
 docker build -t postgres-sample .
 docker run --name postgres-container -p 5432:5432 -d postgres-sample
 
-cd .. || exit
-./mvnw -ntp -Dhttp.keepAlive=false \
+cd ../system || exit
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q -f system/pom.xml clean package liberty:create liberty:install-feature liberty:deploy
-./mvnw -ntp -f system/pom.xml liberty:start
+    -q clean package liberty:create liberty:install-feature liberty:deploy
+mvn -ntp liberty:start
 
-cd module-jwt || exit
+cd ../module-jwt || exit
 
 if [[ -e ./src/main/java/io/openliberty/deepdive/rest/health ]]; then
     rm -fr ./src/main/java/io/openliberty/deepdive/rest/health
@@ -152,17 +152,18 @@ cp ../module-health-checks/src/main/java/io/openliberty/deepdive/rest/health/Rea
 cp ../module-metrics/src/main/liberty/config/server.xml ./src/main/liberty/config/server.xml
 cp ../module-metrics/src/main/java/io/openliberty/deepdive/rest/SystemResource.java ./src/main/java/io/openliberty/deepdive/rest/SystemResource.java
 
-cd .. || exit
-./mvnw -ntp -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q -f module-jwt/pom.xml clean package liberty:create liberty:install-feature liberty:deploy
+    -q clean package liberty:create liberty:install-feature liberty:deploy
 
-./mvnw -ntp -f module-jwt/pom.xml liberty:start
+mvn -ntp liberty:start
 
 sleep 20
 
 echo ===== Test module-health-checks =====
+
+cd ../module-jwt || exit
 
 curl http://localhost:9080/health/started | grep "\"status\":" || exit 1
 curl http://localhost:9080/health/live | grep "\"status\":" || exit 1
@@ -188,9 +189,10 @@ curl -k --user bob:bobpwd https://localhost:9443/metrics\?scope=application | gr
 
 echo ===== Stop all processes
 
-./mvnw -ntp -f module-jwt/pom.xml liberty:stop 
+mvn -ntp liberty:stop 
 
-./mvnw -ntp -f system/pom.xml liberty:stop
+cd ../system
+mvn -ntp liberty:stop
 
 docker stop postgres-container
 docker rm postgres-container
@@ -198,15 +200,13 @@ docker rm postgres-container
 
 echo ===== Test module-testcontainers =====
 
-cd module-jwt
+cd ../module-jwt
 
 cp ../module-kubernetes/src/main/liberty/config/server.xml ./src/main/liberty/config/server.xml
 cp ../module-kubernetes/Dockerfile .
 docker pull -q icr.io/appcafe/open-liberty:full-java17-openj9-ubi 
 
-cd ..
-./mvnw -f module-jwt/pom.xml -ntp package
-cd module-jwt
+mvn -ntp package
 docker build -t liberty-deepdive-inventory:1.0-SNAPSHOT .
 docker images
 docker ps 
@@ -228,12 +228,10 @@ cp ../module-testcontainers/src/test/java/it/io/openliberty/deepdive/rest/System
 cp ../module-testcontainers/src/test/resources/log4j.properties ./src/test/resources
 cp ../module-testcontainers/pom.xml .
 
-cd ..
-./mvnw -ntp -f module-jwt/pom.xml verify -Dtest.protocol=http
+mvn -ntp verify -Dtest.protocol=http
 
 echo ===== Test module-kubernetes =====
 
-cd module-jwt
 cp ../module-kubernetes/inventory.init.yaml .
 cp ../module-kubernetes/inventory.yaml .
 
@@ -245,9 +243,7 @@ minikube status
 #kubectl config view
 eval "$(minikube docker-env)"
 
-cd ..
-./mvnw -f module-jwt/pom.xml package
-cd module-jwt
+mvn package
 docker build -t liberty-deepdive-inventory:1.0-SNAPSHOT .
 docker images
 docker ps 
