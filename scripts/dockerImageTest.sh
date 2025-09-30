e0aaad5791a5778465efba3989eac9bebde2c313
Gilbert Kwan <gkwan@ca.ibm.com>
2025-09-30 11:29:33 -0400

    Change to use image from cp.stg.icr.io/cp/olc

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -24,19 +24,19 @@ cat module-jwt/pom.xml
 cat module-testcontainers/pom.xml
 
 if [[ "$OL_LEVEL" != "" ]]; then
-  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java17-openj9-ubi;g" module-kubernetes/Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java17-openj9-ubi;g" module-kubernetes/Dockerfile
 else
-  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java17-openj9-ubi;g" module-kubernetes/Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java17-openj9-ubi;g" module-kubernetes/Dockerfile
 fi
 cat module-kubernetes/Dockerfile
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin cp.stg.icr.io
 if [[ "$OL_LEVEL" != "" ]]; then
-  docker pull -q "cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java17-openj9-ubi"
-  echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java17-openj9-ubi"
+  docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java17-openj9-ubi"
+  echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java17-openj9-ubi"
 else
-  docker pull -q "cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java17-openj9-ubi"
-  echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java17-openj9-ubi"
+  docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java17-openj9-ubi"
+  echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java17-openj9-ubi"
 fi
 
 ../scripts/testApp.sh
