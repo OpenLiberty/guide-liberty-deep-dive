d79119f18d8751a9d3f6c542d05898535b264593
Siaa G <siaa.gor@ibm.com>
2026-04-29 23:00:20 -0400

    work in progress

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -172,7 +172,6 @@ curl http://localhost:9080/health/ready | grep "\"status\":\"UP\"" || exit 1
 
 echo ===== Test client REST API =====
 RESPONSE=$(curl --no-progress-meter -k --user bob:bobpwd -X POST 'https://localhost:9443/inventory/api/systems/client/localhost')
-echo "$RESPONSE"
 echo "$RESPONSE" | grep "was added" || exit 1
 
 curl 'http://localhost:9080/inventory/api/systems' | grep "\"heapSize\":" || exit 1
