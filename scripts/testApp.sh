12139c2e5c46aef3ec395f99788e01490bf8a17f
Diya Prathap <Diya.Prathap@ibm.com>
2026-09-17 21:54:21 +0530

    Fix module-kubernetes docker build: disable BuildKit and cd to correct directory

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -245,6 +245,7 @@ eval "$(minikube docker-env)"
 ssh-keyscan -p 32768 127.0.0.1 >> ~/.ssh/known_hosts 2>/dev/null || true
 export DOCKER_BUILDKIT=0
 
+cd ../module-kubernetes
 mvn package
 docker build -t liberty-deepdive-inventory:1.0-SNAPSHOT .
 docker images
