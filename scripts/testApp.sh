1b2eb36ae7e271a53b7eb7e3a727c007a8030ea3
Diya Prathap <Diya.Prathap@ibm.com>
2026-09-17 18:46:47 +0530

    Fix SSH host key verification failure in Minikube Docker env

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -242,6 +242,7 @@ minikube status
 #kubectl get services --all-namespaces
 #kubectl config view
 eval "$(minikube docker-env)"
+ssh-keyscan -p 32768 127.0.0.1 >> ~/.ssh/known_hosts 2>/dev/null || true
 
 mvn package
 docker build -t liberty-deepdive-inventory:1.0-SNAPSHOT .
