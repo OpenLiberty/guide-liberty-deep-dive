ce66320976b9a055bb14444cdc150b721ea864a5
Diya Prathap <Diya.Prathap@ibm.com>
2026-09-22 23:22:05 +0530

    Revert "Merge branch 'prod' into staging"
    This reverts commit 20cd4ae7315eb93c7183cb78a0f19084d0072b68, reversing
    changes made to 92117cf24c07aaffaef039f4c146b0bab2933efe.

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -236,12 +236,15 @@ cp ../module-kubernetes/inventory.init.yaml .
 cp ../module-kubernetes/inventory.yaml .
 
 #./../scripts/startMinikube.sh
+mvn -ntp package
 minikube start --force
 minikube status
 #kubectl cluster-info
 #kubectl get services --all-namespaces
 #kubectl config view
 eval "$(minikube docker-env)"
+ssh-keyscan -p 32768 127.0.0.1 >> ~/.ssh/known_hosts 2>/dev/null || true
+export DOCKER_BUILDKIT=0
 
 minikube image build -t liberty-deepdive-inventory:1.0-SNAPSHOT .
 
